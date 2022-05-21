3e7ee670bf5a142da8d417f8ee25aa31e01c158c
Farhan <oyisamfarhan@gmail.com>
2022-05-21 22:10:44 +0700

    diagram 2

--- js/diagram.js
+++ js/diagram.js
@@ -16,13 +16,20 @@ $.ajax({
         var i = 0;
         uniqueLevels.forEach(element => {
             $("#levelDiagram1").append(`<option value="${i}">${element}</option>`);
+            $("#levelDiagram2").append(`<option value="${i}">${element}</option>`);
             i++;
         });
 
-        // jika ada get level
-        if ($_GET['level']) {
-            var level = $_GET['level'];
+        // jika ada get level pada diagram 1
+        if ($_GET['levelD1']) {
+            var level = $_GET['levelD1'];
             $("#levelDiagram1").val(level);
+        }
+
+        // jika ada get level pada diagram 2
+        if ($_GET['levelD2']) {
+            var level = $_GET['levelD2'];
+            $("#levelDiagram2").val(level);
         }
     }
 })
@@ -34,13 +41,18 @@ for (var i = 0; i < parts.length; i++) {
     $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
 }
 
-var idLevel = $_GET['level'];
+// DIAGRAM 1 =============================================================
+if ($_GET['levelD1']) {
+    var idLevelD1 = $_GET['levelD1'];
+} else {
+    var idLevelD1 = 0;
+}
 
 $("#levelDiagram1Submit").click(function(){
     // ambil nilai dari input
-    idLevel = $("#levelDiagram1").val();
+    idLevelD1 = $("#levelDiagram1").val();
 
-    location.href = "./diagram.php?level=" + idLevel;
+    location.href = "./diagram.php?levelD1=" + idLevelD1;
 });
 
 // diagram 1 (level dan tanggal)
@@ -71,34 +83,30 @@ $.ajax({
         var uniqueLevel = [...new Set(level)];
         // console.log(uniqueLevel);
 
-        // hitung jumlah level per tanggal (perhari) dan per level (perhari) 
-        var jumlahLevelPerTanggal = [];
-        var jumlahLevelPerLevel = [];
-        uniqueTanggal.forEach(tanggal => {
-            var jumlahLevelPerTanggalPerLevel = [];
-            uniqueLevel.forEach(level => {
-                var jumlahLevel = 0;
+        // hitung jumlah data per tanggal (perhari) dan per level
+        var jumlahDataPerLevel = [];
+        uniqueLevel.forEach(level => {
+            var jumlahDataPerTanggal = [];
+            uniqueTanggal.forEach(tanggal => {
+                var jumlah = 0;
                 data.forEach(element => {
-                    var date = element.tanggal;
-                    var onlyDate = date.split(" ");
-                    var onlyDate = onlyDate[0];
-                    if (onlyDate == tanggal && element.level == level) {
-                        jumlahLevel++;
+                    if (element.level == level && element.tanggal.includes(tanggal)) {
+                        jumlah++;
                     }
                 });
-                jumlahLevelPerTanggalPerLevel.push(jumlahLevel);
+                jumlahDataPerTanggal.push(jumlah);
             });
-            jumlahLevelPerTanggal.push(jumlahLevelPerTanggalPerLevel);
+            jumlahDataPerLevel.push(jumlahDataPerTanggal);
         });
 
         const dataDiagram = {
             labels: uniqueTanggal,
             datasets: [
                 {
-                    label: uniqueLevel[idLevel],
+                    label: uniqueLevel[idLevelD1],
                     backgroundColor: 'rgb(255, 99, 132)',
                     borderColor: 'rgb(255, 99, 132)',
-                    data: jumlahLevelPerTanggal[idLevel],
+                    data: jumlahDataPerLevel[idLevelD1],
                 }
             ]
         };
@@ -110,7 +118,109 @@ $.ajax({
         };
         
         const myChart = new Chart(
-            document.getElementById('myChart'),
+            document.getElementById('diagram1'),
+            config
+        );
+    }
+})
+
+
+// DIAGRAM 2 =============================================================
+if ($_GET['levelD2']) {
+    var idLevelD2 = $_GET['levelD2'];
+} else {
+    var idLevelD2 = 0;
+}
+
+$("#levelDiagram2Submit").click(function(){
+    // ambil nilai dari input
+    idLevelD2 = $("#levelDiagram2").val();
+
+    location.href = "./diagram.php?levelD2=" + idLevelD2;
+});
+
+// diagram 2 (level dan tanggal) menurut jenisnya
+$.ajax({
+    url: "./getAll.php",
+    type: "POST",
+    dataType: "json",
+    success: function(data) {
+        const tanggal = [];
+        const level = [];
+        const jenis = [];
+        data.forEach(element => {
+            // date without time
+            var date = element.tanggal;
+            var onlyDate = date.split(" ");
+            var onlyDate = onlyDate[0];
+            tanggal.push(onlyDate);
+
+            // level
+            level.push(element.level);
+
+            // jenis
+            jenis.push(element.jenis);
+        });
+
+        // unikkan daftar tanggal
+        var uniqueTanggal = [...new Set(tanggal)];
+        // drop tanggal 0000-00-00
+        uniqueTanggal.shift();
+
+        // unikkan daftar level
+        var uniqueLevel = [...new Set(level)];
+
+        // unikkan daftar jenis
+        var uniqueJenis = [...new Set(jenis)];
+        // drop jenis null
+        uniqueJenis.shift();
+
+        // hitung jumlah data per tanggal (perhari) dan per level dan jenis
+        var jumlahDataPerLevelJenis = [];
+        uniqueJenis.forEach(jenis => {
+            var jumlahDataPerLevel = [];
+            uniqueLevel.forEach(level => {
+                var jumlahDataPerTanggal = [];
+                uniqueTanggal.forEach(tanggal => {
+                    var jumlah = 0;
+                    data.forEach(element => {
+                        if (element.level == level && element.tanggal.includes(tanggal) && element.jenis == jenis) {
+                            jumlah++;
+                        }
+                    });
+                    jumlahDataPerTanggal.push(jumlah);
+                });
+                jumlahDataPerLevel.push(jumlahDataPerTanggal);
+            });
+            jumlahDataPerLevelJenis.push(jumlahDataPerLevel);
+        });
+
+        const dataDiagram = {
+            labels: uniqueTanggal,
+            datasets: [
+                {
+                    label: 'Crash',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: jumlahDataPerLevelJenis[0][idLevelD2],
+                },
+                {
+                    label: 'Moon',
+                    backgroundColor: 'rgb(54, 162, 235)',
+                    borderColor: 'rgb(54, 162, 235)',
+                    data: jumlahDataPerLevelJenis[1][idLevelD2],
+                },
+            ]
+        };
+        
+        const config = {
+            type: 'line',
+            data: dataDiagram,
+            options: {}
+        };
+        
+        const myChart = new Chart(
+            document.getElementById('diagram2'),
             config
         );
     }
